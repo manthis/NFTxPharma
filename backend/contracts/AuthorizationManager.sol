497aba2892dc0785538a8f6282f6db5c16c4a3bf
Maxime AUBURTIN <maxime@auburt.in>
2024-02-11 19:47:27 +0100

    Updated Solidity compiler version.

--- backend/contracts/AuthorizationManager.sol
+++ backend/contracts/AuthorizationManager.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.20;
+pragma solidity ^0.8.18;
 
 import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
