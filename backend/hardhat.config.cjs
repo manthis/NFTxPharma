bf644cff13e33f9e2ccbb37410b2a5e425aeaef5
Maxime AUBURTIN <maxime@auburt.in>
2024-02-12 21:11:50 +0100

    Enabled gas reporter on test

--- backend/hardhat.config.cjs
+++ backend/hardhat.config.cjs
@@ -1,6 +1,11 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("hardhat-gas-reporter");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
     solidity: "0.8.20",
+    gasReporter: {
+        enabled: true,
+        currency: "USD",
+    },
 };
