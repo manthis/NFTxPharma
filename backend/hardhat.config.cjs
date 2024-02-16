bb3fbd3b85768a6461ab9e469e88f128c1e5a756
Maxime AUBURTIN <maxime@auburt.in>
2024-02-16 01:50:21 +0100

    Added possibility to deploy on Sepolia. Also added Etherscan API key to follow transactions on their website.

--- backend/hardhat.config.cjs
+++ backend/hardhat.config.cjs
@@ -4,6 +4,21 @@ require("hardhat-gas-reporter");
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
     solidity: "0.8.20",
+    networks: {
+        sepolia: {
+            url: `https://eth-sepolia.g.alchemy.com/v2/JVFHW5c5MXnRUxytlY26K8jE8CYtzMUb`,
+            accounts: [
+                `0x07df9d114b79521bc10ba50e9d026a62b2fec4913ecb28c0cf74cc69f0e87b90`,
+            ],
+            chainId: 11155111,
+            blockConfirmations: 6,
+        },
+    },
+    etherscan: {
+        apiKey: {
+            sepolia: "2NBVE5FYYTPI9WNQRW4ZX9GQAUE9F86EK3",
+        },
+    },
     gasReporter: {
         enabled: true,
         currency: "USD",
