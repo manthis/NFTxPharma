571318a62e5bedfcfb3822e78a3219d0a6f230b8
Maxime AUBURTIN <maxime@auburt.in>
2024-02-16 16:05:40 +0100

    Updated deployment script

--- backend/scripts/deploy.js
+++ backend/scripts/deploy.js
@@ -16,20 +16,38 @@ async function main() {
     const NFTXP_BASEURI = 'ipfs://QmV9w4bXjS5k5JLs5mZ6q2sQwqNqZc2y4HnF7f4b7v4b7/'; // TODO to be edited
     const NFTXM_BASEURI = 'ipfs://'; // TODO to be edited
 
+    // Patient smart contract
+    const PatientContract = await hre.ethers.getContractFactory('Patient');
+    const patientContract = await PatientContract.deploy(getPatientsHexMerkleRoot());
+    await patientContract.waitForDeployment();
+    console.log('Patient contract deployed to:', patientContract.target);
+
+    // Pharmacy smart contract
+    const PharmacyContract = await hre.ethers.getContractFactory('Pharmacy');
+    const pharmacyContract = await PharmacyContract.deploy(getPharmaciesHexMerkleRoot());
+    await pharmacyContract.waitForDeployment();
+    console.log('Pharmacy contract deployed to:', pharmacyContract.target);
+
+    // Doctor smart contract
+    const DoctorContract = await hre.ethers.getContractFactory('Doctor');
+    const doctorContract = await DoctorContract.deploy(getDoctorsHexMerkleRoot());
+    await doctorContract.waitForDeployment();
+    console.log('Doctor contract deployed to:', doctorContract.target);
+
     // SocialSecurity Smart Contract
     const SocialSecurityContract = await hre.ethers.getContractFactory('SocialSecurity');
     const socialSecurityContract = await SocialSecurityContract.deploy(
         NFTXP_BASEURI,
-        getDoctorsHexMerkleRoot(),
-        getPatientsHexMerkleRoot(),
-        getPharmaciesHexMerkleRoot(),
+        doctorContract.target,
+        patientContract.target,
+        pharmacyContract.target,
     );
     await socialSecurityContract.waitForDeployment();
     console.log('SocialSecurity contract deployed to:', socialSecurityContract.target);
 
     // Laboratory smart contract
     const LaboratoryContract = await ethers.getContractFactory('Laboratory');
-    const laboratoryContract = await LaboratoryContract.deploy(NFTXM_BASEURI, getPharmaciesHexMerkleRoot());
+    const laboratoryContract = await LaboratoryContract.deploy(NFTXM_BASEURI, pharmacyContract.target);
     laboratoryContract.waitForDeployment();
     console.log('Laboratory contract deployed to:', laboratoryContract.target);
 
@@ -50,8 +68,8 @@ async function main() {
     const ExchangerContract = await ethers.getContractFactory('Exchanger');
     const exchangerContract = await ExchangerContract.deploy(
         laboratoryContract.target,
-        getPharmaciesHexMerkleRoot(),
-        getPatientsHexMerkleRoot(),
+        pharmacyContract.target,
+        patientContract.target,
     );
     exchangerContract.waitForDeployment();
     console.log('Exchanger contract deployed to:', exchangerContract.target);
